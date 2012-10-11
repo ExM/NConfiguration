eba67049ea542ee17b52724a13b001fbddd71f05
ExM <ExplorerMachine@gmail.com>
2012-10-11 23:15:37 +0400

    search including files

--- Configuration/Xml/Joining/SettingsLoader.cs
+++ Configuration/Xml/Joining/SettingsLoader.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Xml;
 using System.IO;
+using System.Linq;
 using System.Xml.Linq;
 using System.Xml.Serialization;
 using System.Collections.Generic;
@@ -79,19 +80,71 @@ namespace Configuration.Xml.Joining
 
 			var rpo = settings as IRelativePathOwner;
 
-
-			foreach(var fCfg in incCfg.FileConfigs)
+			foreach (var fCfg in incCfg.FileConfigs)
+			{
 				if (Path.IsPathRooted(fCfg.Path))
 					LoadSettingsByXmlFile(fCfg.Path, fCfg.Required);
 				else
 				{
 					if (rpo == null)
 						throw new InvalidOperationException("can not be searched for a relative path because the settings do not provide an absolute path");
-					//TODO
 
+					var found = SearchXmlSettings(rpo.RelativePath, fCfg.Path, fCfg.Search);
+
+					if (fCfg.Required && found.Count == 0)
+						throw new ApplicationException(string.Format("XML configuration '{0}' not found in '{1}'", fCfg.Path, rpo.RelativePath));
+
+					if (found.Count > 1)
+					{
+						if (fCfg.Include == IncludeMode.First)
+							found = new List<XmlFileSettings>() { found.First() };
+						else if (fCfg.Include == IncludeMode.Last)
+							found = new List<XmlFileSettings>() { found.Last() };
+					}
+
+					foreach (var item in found)
+						LoadSettings(item);
 				}
+			}
 
 			return this;
+		}
+
+		private List<XmlFileSettings> SearchXmlSettings(string basePath, string fileName, SearchMode mode)
+		{
+			var result = new List<XmlFileSettings>();
+
+			if (mode == SearchMode.Up)
+				basePath = Path.GetDirectoryName(basePath);
+
+			while(true)
+			{
+				try
+				{
+					if (!Directory.Exists(basePath))
+						break;
+					var fullPath = Path.Combine(basePath, fileName);
+					if (File.Exists(fullPath))
+					{
+						var item = new XmlFileSettings(fullPath);
+						result.Add(item);
+						var incCfg = item.TryLoad<IncludeConfig>(false);
+						if (incCfg != null && incCfg.FinalSearch)
+							break;
+					}
+
+					if (mode == SearchMode.Exact)
+						break;
+
+					basePath = Path.GetDirectoryName(basePath);
+				}
+				catch (UnauthorizedAccessException)
+				{
+					continue;
+				}
+			}
+
+			return result;
 		}
 
 		private void LoadSettingsByXmlFile(string fileName, bool required)
