dc9aec0d327b2b7ae6d258e2c7c0d505cbded287
ExM <ExplorerMachine@gmail.com>
2013-02-10 23:16:46 +0400

    test navigate in BsonDocument

---
+++ Configuration.MongoDB.Tests/BsonDocumentSettings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Configuration.GenericView;
+using MongoDB.Bson;
+
+namespace Configuration.MongoDB
+{
+	public class BsonDocumentSettings: BsonSettings
+	{
+		private List<KeyValuePair<string, BsonDocument>> _docs = new List<KeyValuePair<string, BsonDocument>>();
+
+		public BsonDocumentSettings(IPlainConverter converter, IGenericDeserializer deserializer)
+			: base(converter, deserializer)
+		{
+		}
+
+		public void Add(KeyValuePair<string, BsonDocument> pair)
+		{
+			_docs.Add(pair);
+		}
+
+		protected override IEnumerable<BsonDocument> GetDocuments(string name)
+		{
+			return _docs.Where(p => p.Key == name).Select(p => p.Value);
+		}
+	}
+}
--- Configuration.MongoDB/ViewDocument.cs
+++ Configuration.MongoDB/ViewDocument.cs
@@ -24,26 +24,97 @@ namespace Configuration.MongoDB
 			if (el == null)
 				return null;
 
-			return ViewCreater.CreateByBsonValue(_converter, ViewCreater.FlatArray(el.Value).FirstOrDefault());
+			return CreateByBsonValue(_converter, FlatArray(el.Value).FirstOrDefault());
 		}
 
 		public IEnumerable<ICfgNode> GetCollection(string name)
 		{
 			return _doc.Elements.Where(el => el.Name == name)
-				.SelectMany(el => ViewCreater.FlatArray(el.Value))
-				.Select(el => ViewCreater.CreateByBsonValue(_converter, el));
+				.SelectMany(el => FlatArray(el.Value))
+				.Select(el => CreateByBsonValue(_converter, el));
 		}
 
 		public IEnumerable<KeyValuePair<string, ICfgNode>> GetNodes()
 		{
 			foreach(var el in _doc.Elements)
-				foreach(var val in ViewCreater.FlatArray(el.Value))
-					yield return new KeyValuePair<string, ICfgNode>(el.Name, ViewCreater.CreateByBsonValue(_converter, val));
+				foreach (var val in FlatArray(el.Value))
+					yield return new KeyValuePair<string, ICfgNode>(el.Name, CreateByBsonValue(_converter, val));
 		}
 
 		public T As<T>()
 		{
 			throw new NotSupportedException("BsonDocument can't contain value");
+		}
+
+		internal static ICfgNode CreateByBsonValue(IPlainConverter converter, BsonValue val)
+		{
+			if (val == null)
+				return null;
+
+			switch (val.BsonType)
+			{
+				case BsonType.Null:
+					return null;
+
+				case BsonType.Document:
+					return new ViewDocument(converter, (BsonDocument)val);
+
+				case BsonType.String:
+					return new ViewPlainField<string>(converter, ((BsonString)val).Value);
+
+				case BsonType.Binary:
+					return new ViewPlainField<byte[]>(converter, ((BsonBinaryData)val).Bytes);
+
+				case BsonType.Boolean:
+					return new ViewPlainField<bool>(converter, ((BsonBoolean)val).Value);
+
+				case BsonType.DateTime:
+					return new ViewPlainField<DateTime>(converter, ((BsonDateTime)val).Value);
+
+				case BsonType.Double:
+					return new ViewPlainField<double>(converter, ((BsonDouble)val).Value);
+
+				case BsonType.Int32:
+					return new ViewPlainField<int>(converter, ((BsonInt32)val).Value);
+
+				case BsonType.Int64:
+					return new ViewPlainField<long>(converter, ((BsonInt64)val).Value);
+
+				case BsonType.JavaScript:
+					return new ViewPlainField<string>(converter, ((BsonJavaScript)val).Code);
+
+				case BsonType.RegularExpression:
+					return new ViewPlainField<string>(converter, ((BsonRegularExpression)val).ToString());
+
+				case BsonType.Symbol:
+					return new ViewPlainField<string>(converter, ((BsonSymbol)val).Name);
+
+				case BsonType.Timestamp:
+					return new ViewPlainField<long>(converter, ((BsonTimestamp)val).Value);
+
+				default:
+					throw new NotSupportedException(string.Format("BSON type {0} not supported", val.BsonType));
+			}
+		}
+
+		internal static IEnumerable<BsonValue> FlatArray(BsonValue val)
+		{
+			if (val == null || val.BsonType == BsonType.Null)
+				yield break;
+
+			if (val.BsonType != BsonType.Array)
+			{
+				yield return val;
+				yield break;
+			}
+
+			foreach (var item in ((BsonArray)val).Values)
+			{
+				foreach (var innerItem in FlatArray(item)) //HACK: remove recursion
+				{
+					yield return innerItem;
+				}
+			}
 		}
 	}
 }
