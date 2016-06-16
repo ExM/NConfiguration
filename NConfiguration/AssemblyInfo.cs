1194bdcca52e93aa5ae1491659ef1efb623fcc01
ExM <ExplorerMachine@gmail.com>
2016-06-16 19:16:24 +0300

    bumpimg version

--- NConfiguration/AssemblyInfo.cs
+++ NConfiguration/AssemblyInfo.cs
@@ -17,5 +17,5 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0-rc1")]
+[assembly: AssemblyVersion("2.2.0")]
+[assembly: AssemblyInformationalVersion("2.2.0")]
