9ac66c8f9f382198d8639c9c4aff0824ab14db55
ExM <explorermachine@gmail.com>
2017-10-11 23:48:39 +0300

    create strong-name assembly

---
+++ packetRestore.ps1
@@ -0,0 +1,4 @@
+$ErrorActionPreference = "Stop"
+$mainFolder = Resolve-Path (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
+$paketExe = "$mainFolder\.paket\paket.exe"
+& "$paketExe" restore
