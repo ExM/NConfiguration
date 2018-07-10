db6c636170c03e2a695474e8f1511b860f2f85c7
Eugene Blinov <tyeidek@gmail.com>
2018-07-10 12:30:44 +0300

    modified TimeSpanParser to have single regex and to run regex match for only one time per value

--- NConfiguration/Serialization/SimpleTypes/Parsing/TimeSpanParser.cs
+++ NConfiguration/Serialization/SimpleTypes/Parsing/TimeSpanParser.cs
@@ -11,21 +11,16 @@ namespace NConfiguration.Serialization.SimpleTypes.Parsing
 			RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled;
 		private const string PatternBase = @"(-?\d+(?:\.\d+)?)";
 
-		private static Regex GetExpression(string unitOfMeasurement)
+		private static readonly KeyValuePair<int, Func<double, TimeSpan>>[] _groupNumberToSpans =
 		{
-			return new Regex($@"{PatternBase}{unitOfMeasurement}", RegularExpressionOptions);
-		}
+			new KeyValuePair<int, Func<double, TimeSpan>>(2, TimeSpan.FromDays),
+			new KeyValuePair<int, Func<double, TimeSpan>>(4, TimeSpan.FromHours),
+			new KeyValuePair<int, Func<double, TimeSpan>>(6, TimeSpan.FromMinutes),
+			new KeyValuePair<int, Func<double, TimeSpan>>(8, TimeSpan.FromSeconds)
+		};
 
 		private static readonly Regex _checkFormatRegex = 
 			new Regex($@"({PatternBase}d)?({PatternBase}h)?({PatternBase}m)?({PatternBase}s)?", RegularExpressionOptions);
-
-		private readonly KeyValuePair<Regex, Func<double, TimeSpan>>[] _regexToSpans =
-		{
-			new KeyValuePair<Regex, Func<double, TimeSpan>>(GetExpression("d"), TimeSpan.FromDays),
-			new KeyValuePair<Regex, Func<double, TimeSpan>>(GetExpression("h"), TimeSpan.FromHours),
-			new KeyValuePair<Regex, Func<double, TimeSpan>>(GetExpression("m"), TimeSpan.FromMinutes),
-			new KeyValuePair<Regex, Func<double, TimeSpan>>(GetExpression("s"), TimeSpan.FromSeconds)
-		};
 
 		public TimeSpan Parse(string rawInput, CultureInfo cultureInfo)
 		{
@@ -39,17 +34,22 @@ namespace NConfiguration.Serialization.SimpleTypes.Parsing
 
 		private TimeSpan ParseShortFormat(string rawInput, CultureInfo cultureInfo)
 		{
-			ValidateShortFormat(rawInput);
+			var match = _checkFormatRegex.Match(rawInput);
+			if (!match.Success || match.Value != rawInput.Trim())
+			{
+				throw new FormatException();
+			}
+
 			var result = TimeSpan.Zero;
 			var hasPositive = false;
 			var hasNegative = false;
-			foreach (var regexToSpan in _regexToSpans)
+			foreach (var groupNumberToSpan in _groupNumberToSpans)
 			{
-				var match = regexToSpan.Key.Match(rawInput);
-				if (match.Success)
+				var value = match.Groups[groupNumberToSpan.Key].Value;
+				if (!String.IsNullOrWhiteSpace(value))
 				{
-					var number = double.Parse(match.Groups[1].Value, cultureInfo);
-					result = result.Add(regexToSpan.Value(number));
+					var number = double.Parse(value, cultureInfo);
+					result = result.Add(groupNumberToSpan.Value(number));
 					hasPositive |= number > 0;
 					hasNegative |= number < 0;
 				}
@@ -59,15 +59,6 @@ namespace NConfiguration.Serialization.SimpleTypes.Parsing
 				return result;
 			}
 			throw new FormatException();
-		}
-
-		private static void ValidateShortFormat(string rawInput)
-		{
-			var match = _checkFormatRegex.Match(rawInput);
-			if (!match.Success || match.Value != rawInput.Trim())
-			{
-				throw new FormatException();
-			}
 		}
 	}
 }
