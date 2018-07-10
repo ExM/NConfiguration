3882a5fa0390483573cf5471cf55826d4e9f6696
Eugene Blinov <tyeidek@gmail.com>
2018-07-10 11:37:20 +0300

    Modified TimeConfig to have single property - different values now handled via separate test cases instead of combination of properties in single instance of config;

--- NConfiguration.Tests/Serialization/ShortFormatTimeSpansTests.cs
+++ NConfiguration.Tests/Serialization/ShortFormatTimeSpansTests.cs
@@ -8,31 +8,23 @@ namespace NConfiguration.Serialization
 	{
 		public class TimeConfig
 		{
-			public TimeSpan Time1 { get; set; }
-
-			public TimeSpan Time2 { get; set; }
-
-			public TimeSpan Time3 { get; set; }
+			public TimeSpan Time { get; set; }
 		}
 
 		public static object[] CaseSets => new object[]
 		{
-			new object[] {"15d", "2h", "15:00:00", TimeSpan.FromDays(15), TimeSpan.FromHours(2), TimeSpan.FromHours(15)}
+			new object[] {"15d", TimeSpan.FromDays(15) },
+			new object[] {"2h", TimeSpan.FromHours(2) },
+			new object[] {"2h30m", new TimeSpan(0, 2, 30, 0) }
 		};
 
 		[TestCaseSource(nameof(CaseSets))]
-		public void WhenXmlContainsShortTextTimes_ThenTheyShouldBeParsedCorrectly(
-			string time1, string time2, string time3,
-			TimeSpan expectedTime1, TimeSpan expectedTime2, TimeSpan expectedTime3)
+		public void WhenXmlContainsShortTextTimes_ThenTheyShouldBeParsedCorrectly(string time, TimeSpan expectedTime)
 		{
-			var root =
-				$@"<Root Time1='{time1}' Time2='{time2}' Time3='{time3}' />".ToXmlView();
+			var root = $@"<Root Time='{time}' />".ToXmlView();
 
 			var item = DefaultDeserializer.Instance.Deserialize<TimeConfig>(root);
-
-			Assert.AreEqual(expectedTime1, item.Time1);
-			Assert.AreEqual(expectedTime2, item.Time2);
-			Assert.AreEqual(expectedTime3, item.Time3);
+			Assert.AreEqual(expectedTime, item.Time);
 		}
 	}
 }
